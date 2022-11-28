fd6e01784d53f887f885941c2b860c38a52934fd
i.shirinov <shirinovilyos6965@gmail.com>
2022-11-28 12:31:03 +0500

    Vazifa №5. almashtirish

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0 )  || (year % 100 == 0 && year % 400 == 0)) {
+            System.out.println("kabisa yili");
+        } else {
+            System.out.println("kabisa yili emas!!!");
+        }
+    }
+
+    public static void main(String[] args) {
+        LeapYearPrinter printer = new LeapYearPrinter();
+        printer.isLeapYear(1932);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,51 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            case 1:
+                System.out.println("Yanvar oyi 31 kundan iborat!!!");
+                break;
+            case 2:
+                System.out.println("Febral oyi 28 kundan iborat!!!");
+                break;
+            case 3:
+                System.out.println("Mart oyi 31 kundan iborat!!!");
+                break;
+            case 4:
+                System.out.println("Aprel oyi 30 kundan iborat!!!");
+                break;
+            case 5:
+                System.out.println("May oyi 31 kundan iborat!!!");
+                break;
+            case 6:
+                System.out.println("Iyun oyi 30 kundan iborat!!!");
+                break;
+            case 7:
+                System.out.println("Iyul oyi 31 kundan iborat!!!");
+                break;
+            case 8:
+                System.out.println("Avgust oyi 31 kundan iborat!!!");
+                break;
+            case 9:
+                System.out.println("Sentyabr oyi 30 kundan iborat!!!");
+                break;
+            case 10:
+                System.out.println("Octyabr oyi 31 kundan iborat!!!");
+                break;
+            case 11:
+                System.out.println("Noyabr oyi 30 kundan iborat!!!");
+                break;
+            case 12:
+                System.out.println("Dekabr oyi 31 kundan iborat!!!");
+                break;
+            default:
+                System.out.println("Bunaqa oy mavjud emas!!!");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        SwitchDaysInMonthPrinter printer=new SwitchDaysInMonthPrinter();
+        printer.amountOfDays(13);
     }
 }
